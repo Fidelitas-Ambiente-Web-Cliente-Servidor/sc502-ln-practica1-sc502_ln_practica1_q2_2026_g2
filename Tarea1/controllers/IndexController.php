fd882f8bfb6332708409040d52c06ea7024e84d4
Hector Viquez <hviquez80118@ufide.ac.cr>
2026-07-12 14:58:36 -0600

    Index

--- Tarea1/controllers/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../models/IndexModel.php';
-class IndexController {
-    public function index(): void {
-        $model=new IndexModel(); $cursos=$model->getAll();
-        $pageTitle='Inicio | Academia Berk'; $activePage='index'; $extraCss='index.css';
-        require __DIR__ . '/../views/index/index.php';
-    }
-}
